c13c9a7b67cc008eb4774fded7282ce0339f5832
Siarhei Kuchuk <Cuchuk.Sergey@gmail.com>
2016-07-03 17:28:46 +0300

    + L4 home task

---
+++ L4/Home Work/L4HomeWork/L4HomeWork/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace L4HomeWork.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Home()
+        {
+            return View("Index");
+        }
+
+        public ActionResult Contacts()
+        {
+            return View("Index");
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ L4/Home Work/L4HomeWork/L4HomeWork/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="L4HomeWork.MvcApplication" Language="C#" %>
