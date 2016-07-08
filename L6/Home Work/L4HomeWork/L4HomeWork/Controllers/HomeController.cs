14cb1a09c797e541de75b44784b22496fef34e80
Siarhei Kuchuk <Cuchuk.Sergey@gmail.com>
2016-07-08 14:36:54 +0300

    + Home Work 6

---
+++ L6/Home Work/L4HomeWork/L4HomeWork/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using L4HomeWork.DAL;
+
+namespace L4HomeWork.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(int? id)
+        {
+            //using (var dbContext = new ShopDalDataContext())
+            //{
+            //    //dbContext.Categories.InsertOnSubmit(new Category() {Name="Mobile Phone"});
+            //    //dbContext.Categories.InsertOnSubmit(new Category() { Name = "Notebook" });
+            //    //dbContext.Categories.InsertOnSubmit(new Category() { Name = "Camera" });
+            //    //dbContext.Categories.InsertOnSubmit(new Category() { Name = "Head Phones" });
+
+            //    //dbContext.Products.InsertOnSubmit(new Product() { Name = "Lumia", Price = 10});
+            //    //dbContext.Products.InsertOnSubmit(new Product() { Name = "Acer", Price = 100 });
+            //    //dbContext.Products.InsertOnSubmit(new Product() { Name = "Asus", Price = 200 });
+
+                
+
+            //   dbContext.ProductCategories.InsertOnSubmit(new ProductCategory() {Category_UID = 1, Product_UID = 1});
+            //    dbContext.ProductCategories.InsertOnSubmit(new ProductCategory() { Category_UID = 1, Product_UID = 2 });
+
+            //    dbContext.ProductCategories.InsertOnSubmit(new ProductCategory() { Category_UID = 2, Product_UID = 2 });
+            //    dbContext.ProductCategories.InsertOnSubmit(new ProductCategory() { Category_UID = 2, Product_UID = 3 });
+
+            //    dbContext.SubmitChanges();
+            //}
+
+
+
+            return View(id);
+        }
+
+        public ActionResult Home()
+        {
+            return View("Index");
+        }
+
+        public ActionResult Contacts()
+        {
+            return View("Index");
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
